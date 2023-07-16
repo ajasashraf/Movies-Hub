60022a514f469df9f372dab3b099bc3725091db1
Ajas Ashraf <ajasashraf7010@gmail.com>
2023-07-16 10:42:01 +0530

    commit changes

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Header from "./components/Header/Header";
+import HomePage from "./components/SearchBar/SearchBar";
+import Footer from "./components/Footer/Footer";
+function App() {
+  return (
+    <div>
+      <Header />
+      <HomePage />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
